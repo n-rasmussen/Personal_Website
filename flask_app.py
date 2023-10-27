8d6c9b2ad5a34911fe486c4ef1af96fcc8966a8b
n.rasmussen <nodrasmu@uwaterloo.ca>
2023-10-26 21:08:56 -0400

    Initial commit

---
+++ flask_app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+
+@app.route('/about/')
+def about():
+	return render_template('about.html')
+
+@app.route('/projects/')
+def projects():
+	return render_template('projects.html')
+
+@app.route('/projects/game/')
+def game():
+	return render_template('game.html')
+
+@app.route('/projects/kNN/')
+def knn():
+	return render_template('knn.html')
+
+@app.route('/projects/regression/')
+def regression():
+	return render_template('regression.html')
+
+@app.route('/projects/ConvNN_MNIST/')
+def MNIST_CNN():
+	return render_template('ConvNN_MNIST.html')
+
+@app.route('/about/resume/')
+def resume():
+	return render_template('resume.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
